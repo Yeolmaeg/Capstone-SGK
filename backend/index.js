d5cd99e98053420994d549c1bffd03157bfd4e32
Yujin Gung <yoojin3117@ewhain.net>
2025-04-06 00:48:38 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const cron = require("node-cron");
 const db = require("./lib/db");
+const userRoutes = require("./routes/userRoutes");
+const scheduleRoutes = require('./routes/scheduleRoutes');
 const placeInfoRoutes = require("./routes/placeInfoRoutes");
+const addressRoutes = require("./routes/addressRoutes");
 const recommendationRoutes = require("./routes/recommendationRoutes");
+const autoScheduleRoutes = require("./routes/autoScheduleRoutes");
 const dotenv = require("dotenv");
+const { generateNextMonthRecurringSchedules } = require("./services/scheduleService");
+
 
 dotenv.config();
 const app = express();
@@ -11,9 +18,12 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/api", placeInfoRouter);
-app.use("/api", recommendationRoutes);
+app.use("/user", userRoutes);
+app.use('/api', scheduleRoutes);
+app.use("/api", placeInfoRoutes);
+app.use("/api/recommendation", recommendationRoutes);
+app.use("/address", addressRoutes);
+app.use("/api", autoScheduleRoutes);
 
 // 서버 실행
 app.listen(PORT, () => {
@@ -23,3 +33,9 @@ app.listen(PORT, () => {
 db.query("SELECT NOW()")
   .then(res => console.log("✅ PostgreSQL Connected!", res.rows[0]))
   .catch(err => console.error("❌ PostgreSQL Connection Error:", err));
+
+// 매달 1일 00시에 반복 일정 생성(시간표 일정)
+cron.schedule("0 0 1 * *", async () => {
+  console.log("📆 매달 반복 일정 생성 시작");
+  await generateNextMonthRecurringSchedules();
+});
