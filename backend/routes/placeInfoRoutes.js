6068cca18b6704fdeadfa2397bce2d385bfb573d
Yujin Gung <yoojin3117@ewhain.net>
2025-04-05 19:41:59 +0900

    Update placeInfoRoutes.js

--- backend/routes/placeInfoRoutes.js
+++ backend/routes/placeInfoRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/placeInfoController");
 
-console.log("placeInfoRoutes 등록됨");
+console.log("✅ placeInfoRoutes 등록됨");
 
 router.post("/placeInfo", controller.getPlaceInfo);
 
