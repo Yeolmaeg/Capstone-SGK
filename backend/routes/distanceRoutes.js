bb45966e3c8c3a6d65fa9e26a176a0f9318c0509
Yeolmaeg <ptgson@gmail.com>
2025-04-06 18:37:30 +0900

    Update distanceRoutes.js (직선거리 GET 지우고 같이 얻는 POST 추가)

--- backend/routes/distanceRoutes.js
+++ backend/routes/distanceRoutes.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const distanceController = require("../controllers/distanceController");
 const travelTimeController = require("../controllers/travelTimeController");
 
-// [기존 GET] 직선 거리만
-router.get("/distance", distanceController.getDistanceInfo);
-
-// [신규 POST] 직선 거리 + 이동수단별 소요 시간
+// 직선 거리 + 이동수단별 소요 시간
 router.post("/distance/detail", travelTimeController.getTravelInfo);
 
 module.exports = router;
