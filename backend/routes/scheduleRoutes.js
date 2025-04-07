69a14bcdde0b736f1a946eaac56ea887b182785d
Yujin Gung <yoojin3117@ewhain.net>
2025-04-07 16:03:56 +0900

    Update scheduleRoutes.js

--- backend/routes/scheduleRoutes.js
+++ backend/routes/scheduleRoutes.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const controller = require("../controllers/scheduleController");
 
-router.get("/schedule", controller.getSchedules);       // 전체 일정 조회
-router.get("/schedule/:id", controller.getScheduleById); // 일정 하나 조회
-router.post("/schedule", controller.addSchedule);       // 일정 추가
-router.delete("/schedule/:id", controller.deleteSchedule); // 일정 삭제
-router.put("/schedule/:id", controller.updateSchedule);     // 일정 수정
-router.patch("/schedule/:id", controller.updateSchedule);
-// router.post("/schedule/from-recommendation", controller.addScheduleFromRecommendation); // 일정 추천
+const scheduleController = require("../controllers/scheduleController");
+
+// 일정 관련
+router.get("/schedule", scheduleController.getSchedules);       // 전체 일정 조회
+router.get("/schedule/:id", scheduleController.getScheduleById); // 일정 하나 조회
+router.post("/schedule", scheduleController.addSchedule);       // 일정 추가
+router.delete("/schedule/:id", scheduleController.deleteSchedule); // 일정 삭제
+router.put("/schedule/:id", scheduleController.updateSchedule);     // 일정 수정
+router.patch("/schedule/:id", scheduleController.updateSchedule);
 
 
 module.exports = router;
