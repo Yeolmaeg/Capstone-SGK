a946971f31e20ae16cc1b8f5a4c264f879818f03
Yujin Gung <yoojin3117@ewhain.net>
2025-04-14 12:43:03 +0900

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/userController");
 
-router.post("/", controller.createUser);         // 사용자 생성
+// router.post("/", controller.createUser);         // 사용자 생성
 router.get("/:id", controller.getUser);           // 단일 사용자 조회
 router.patch("/:id", controller.updateUser);      // 학교/시간표 수정
 router.delete("/:id", controller.deleteUser);     // 사용자 삭제
+router.post('/signup', controller.signup);        // 회원가입
+router.post('/login', controller.login);          // 로그인
 
 module.exports = router;
 
