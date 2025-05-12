62ca651703bbfb37a3d3f979079807c8c6b47c7d
DESKTOP-0C0AIIE\yooji <yoojin3117@ewhain.net>
2025-05-12 18:19:55 +0900

    색상추가가

--- backend/controllers/autoScheduleController.js
+++ backend/controllers/autoScheduleController.js
@@ -14,7 +14,7 @@ exports.autoAddFromPlaceName = async (req, res) => {
     // 1. 장소 정보 받아오기
     const place = await getPlaceInfoFromPerplexity(place_name);
 
-// 2. 일정 자동 생성
+    // 2. 일정 자동 생성
     const utcNow = new Date();
     const kstNow = new Date(utcNow.getTime() + 9 * 60 * 60 * 1000);
 
@@ -24,7 +24,8 @@ exports.autoAddFromPlaceName = async (req, res) => {
       user_id,
       time: kstNow,
       place,
-      source: "from_place"
+      source: "from_place",
+      color: "#d1ebb6"
     });
 
     // 3. 응답 반환 (result 아님!)
