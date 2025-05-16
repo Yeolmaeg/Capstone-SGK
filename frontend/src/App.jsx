d3c4a0298fe94535902b218856509b98a08ac407
K00JAY <triggerthefever@ewhain.net>
2025-05-16 12:20:49 +0900

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 import UnivSelectScreen from "./screens/UnivSelectScreen";
 import TimetableUploadScreen from "./screens/TimetableUploadScreen";
 import DateSelectionScreen from "./screens/DateSelectionScreen"; 
 import ScanningScreen from "./screens/ScanningScreen";
 import ScannedScreen from "./screens/ScannedScreen";
+import MyPageScreen from "./screens/MyPageScreen";
+import MyPageEditScreen from "./screens/MyPageEditScreen";
 import CalendarViewScreen from "./screens/CalendarViewScreen";
 import TimelineViewScreen from "./screens/TimelineViewScreen";
 import AddScheduleScreen from "./screens/AddScheduleScreen";
@@ -17,24 +20,27 @@ import SelectLocationScreen from "./screens/SelectLocationScreen";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<UnivSelectScreen />} />
-        <Route path="/timetable-upload" element={<TimetableUploadScreen />} />
-        <Route path="/dateselection" element={<DateSelectionScreen />} />
-        <Route path="/scanning" element={<ScanningScreen />} />
-        <Route path="/scanned" element={<ScannedScreen />} />
-        <Route path="/calendarview" element={<CalendarViewScreen />} />
-        <Route path="/timelineview" element={<TimelineViewScreen />} />
-        <Route path="/addschedule" element={<AddScheduleScreen />} />
-        <Route path="/scheduledetail" element={<ScheduleDetailScreen />} />
-        <Route path="/editschedule" element={<EditScheduleScreen />} />
-        <Route path="/recommendation" element={<RecommendationScreen />} />
-        <Route path="/recommendationdetail" element={<RecommendationDetailScreen />} />
-        <Route path="/editrecommendation" element={<EditRecommendationScreen />} />
-        <Route path="/selectlocation" element={<SelectLocationScreen />} />
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+          <Route path="/univ-select" element={<UnivSelectScreen />} />
+          <Route path="/timetable-upload" element={<TimetableUploadScreen />} />
+          <Route path="/dateselection" element={<DateSelectionScreen />} />
+          <Route path="/scanning" element={<ScanningScreen />} />
+          <Route path="/scanned" element={<ScannedScreen />} />
+          <Route path="/mypage" element={<MyPageScreen />} />
+          <Route path="/editmypage" element={<MyPageEditScreen />} />
+          <Route path="/calendarview" element={<CalendarViewScreen />} />
+          <Route path="/timelineview" element={<TimelineViewScreen />} />
+          <Route path="/addschedule" element={<AddScheduleScreen />} />
+          <Route path="/scheduledetail" element={<ScheduleDetailScreen />} />
+          <Route path="/editschedule" element={<EditScheduleScreen />} />
+          <Route path="/recommendation" element={<RecommendationScreen />} />
+          <Route path="/recommendationdetail" element={<RecommendationDetailScreen />} />
+          <Route path="/editrecommendation" element={<EditRecommendationScreen />} />
+          <Route path="/selectlocation" element={<SelectLocationScreen />} />
+        </Routes>
+      </Router>
   );
 };
 
