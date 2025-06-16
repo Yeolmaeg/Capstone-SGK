2f7a130fe5c982aafb6ef617dce3b1b90e43cfad
Yujin Gung <yoojin3117@ewhain.net>
2025-06-16 23:34:30 +0900

    Update redisClient.js

--- backend/services/redisClient.js
+++ backend/services/redisClient.js
@@ -1,6 +1,11 @@
 const Redis = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+  tls: {},
+});
 
 redis.on('connect', () => {
   console.log('✅ Redis connected');
