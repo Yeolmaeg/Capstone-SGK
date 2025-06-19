5ca016e8068d40f2c05df3e552f9a8cbb443a269
DESKTOP-0C0AIIE\yooji <yoojin3117@ewhain.net>
2025-06-19 16:11:05 +0900

    vercel 추가

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:3000"], // 여러 개도 가능
+  origin: ["http://localhost:5173", "http://localhost:3000", "https://capstone-brown-rho.vercel.app"], // 여러 개도 가능
   credentials: true
 }));
 app.use(express.json());
